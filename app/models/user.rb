6154db4e3ecc18bdd28bfb178d43d4fee8324392
Jackson Stewart <jts3304@tamu.edu>
2024-09-05 17:12:31 -0500

    validate presence of fields; add container def

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-#  has_many :posts
-#  has_many :comments
-#add another comment. Yeah
+  has_many :posts
+  has_many :comments
 
-
+  validates_presence_of :first_name, :last_name
 end
